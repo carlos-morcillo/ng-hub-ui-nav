59fe9ed1e6eed059db4ae609437dbf0ebef922d6
Carlos Morcillo <carlos.morcillo@me.com>
2026-04-01 10:56:37 +0200

    refactor: simplify public API export paths and sort entries alphabetically

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,27 +3,27 @@
  */
 
 // Models
-export * from './lib/models/nav-item.model';
-export * from './lib/models/nav-config.model';
-export * from './lib/models/nav-template-context.model';
-export * from './lib/models/nav-events.model';
-export * from './lib/models/nav-panel-state.model';
+export * from './models/nav-config.model';
+export * from './models/nav-events.model';
+export * from './models/nav-item.model';
+export * from './models/nav-panel-state.model';
+export * from './models/nav-template-context.model';
 
 // Services
-export * from './lib/services/nav-config.service';
-export * from './lib/services/nav-state.service';
+export * from './services/nav-config.service';
+export * from './services/nav-state.service';
 
 // Directives
-export * from './lib/directives/nav-start.directive';
-export * from './lib/directives/nav-end.directive';
-export * from './lib/directives/nav-item-template.directive';
-export * from './lib/directives/nav-scroll-spy.directive';
-export * from './lib/directives/nav-scroll-spy-section.directive';
+export * from './directives/nav-end.directive';
+export * from './directives/nav-item-template.directive';
+export * from './directives/nav-scroll-spy-section.directive';
+export * from './directives/nav-scroll-spy.directive';
+export * from './directives/nav-start.directive';
 
 // Components
-export * from './lib/components/nav/nav.component';
-export * from './lib/components/nav-item/nav-item.component';
-export * from './lib/components/nav-item-list/nav-item-list.component';
-export * from './lib/components/nav-separator/nav-separator.component';
-export * from './lib/components/nav-panel/nav-panel.component';
-export * from './lib/components/nav-panel-container/nav-panel-container.component';
+export * from './components/nav-item-list/nav-item-list.component';
+export * from './components/nav-item/nav-item.component';
+export * from './components/nav-panel-container/nav-panel-container.component';
+export * from './components/nav-panel/nav-panel.component';
+export * from './components/nav-separator/nav-separator.component';
+export * from './components/nav/nav.component';
